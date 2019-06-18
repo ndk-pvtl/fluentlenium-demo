59fa8c8d4a8a918a16151dab47edc4b9b6f3e24b
Dmitry N <dnurmuradov@pivotal.io>
2019-06-18 08:30:12 -0500

    Add headless mode

--- src/test/java/io/pivotal/fluentleum/demo/BaseChromeTest.java
+++ src/test/java/io/pivotal/fluentleum/demo/BaseChromeTest.java
@@ -1,7 +1,9 @@
 package io.pivotal.fluentleum.demo;
 
 import org.fluentlenium.adapter.junit.jupiter.FluentTest;
+import org.fluentlenium.configuration.FluentConfiguration;
 
+@FluentConfiguration(webDriver="chrome", capabilities = "{\"chromeOptions\": {\"args\": [\"headless\",\"disable-gpu\"]}}")
 public class BaseChromeTest extends FluentTest {
 
 }
